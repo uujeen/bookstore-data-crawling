48875e29e25eface255057d34af756da136ae925
uujeen <rkddmlwls2@gmail.com>
2022-12-06 16:10:29 +0900

    Json 파싱 및 책들의 각각 페이지 URL 추출

--- src/crawlingReview.js
+++ src/crawlingReview.js
@@ -3,7 +3,6 @@ const cheerio = require('cheerio');
 const fs = require('fs');
 const path = require('path');
 const jsonData = require('./data/list_2021.json');
-
 /**
  * 리뷰데이터 크롤링
  * 
@@ -11,47 +10,39 @@ const jsonData = require('./data/list_2021.json');
 async function crawlingReview() {
     // const browser = await puppeteer.launch();
     // const page = await browswer.newPage();
-    /*
-    let fileName = 'list_2021.csv'
-    const csvPath = path.join(__dirname,'..','src','data-csv',fileName);
 
-    //console.log(csvPath);
-    const csv = fs.readFileSync(csvPath,'utf-8');
-    //console.log(csv);
-    const rows = csv.split('\r\n');
-    if(rows[rows.length-1] === ''){
-        rows.pop();
-    }
-    //console.log(rows);
-    let result = [];
-    let columnTitle = [];
-    for(const i in rows){
-        const row = rows[i];
-        const data = row.split(',');
-        if(i==='0'){
-            columnTitle = data;
-        } else {
-            let row_data = {};
-            for(const index in columnTitle){
-                const title = columnTitle[index];                
-                row_data[title] = data[index];
-                
-            }
-            result.push(row_data);
-        }
-    }
-    console.log(result);
-    */
-    let keys = Object.keys(jsonData);
-    for (const i in keys){
-        let key = keys[i]
-        console.log(JSON.stringify(jsonData[key]));
-    }
-    //console.log(rows);
+    let keys = Object.keys(jsonData.data.bestSeller);
+    //console.log(keys);
+    jsonData.data.bestSeller.map((el) =>{
 
-    //const pageUrl = "https://product.kyobobook.co.kr/detail/${}"
+        const booksSerialNumber = el.saleCmdtid;// 책 리뷰에 접근하기 위한 시리얼 넘버
+        //console.log(el.cmdtName, ',', el.saleCmdtid, ',', el.buyRevwNumc, ',', el.buyRevwRvgr);
+        
+        //console.log(booksSerialNumber); 
+
+        const pageUrl = 'https://product.kyobobook.co.kr/detail/' + booksSerialNumber;
+        console.log(pageUrl); // 리스트업 되어 있는 책들의 리뷰 데이터가 들어있는 URL
+        //visitUrl(pageUrl);
+    })
+
 }
 
+async function visitUrl (url) {
+    const browser = await puppeteer.launch(); // ❶ 헤드리스 브라우저 실행
+    const page = await browser.newPage();     // ❷ 브라우저에 새 페이지 생성
+  
+    const pageUrl = "https://product.kyobobook.co.kr/bestseller/total?period=004";
+    await page.goto(pageUrl, {
+      // ❸ 모든 네트워크 연결이 500ms 이상 유휴 상태가 될 때까지 기다림
+      waitUntil: 'networkidle0'
+    });
+
+    const body = await page.content();
+  
+    await page.close();
+    await browser.close();
+
+}
 crawlingReview();
 
 // #ReviewList1 > div.tab_wrap.type_sm > div.tab_content > div > div.pagination > div > a:nth-child(1)

05ec9d429d6ad681e48db4272ec05062e4fe2c7a
uujeen <rkddmlwls2@gmail.com>
2022-12-07 23:08:11 +0900

    review 첫번째 페이지 스크랩핑

--- src/crawlingReview.js
+++ src/crawlingReview.js
@@ -11,6 +11,7 @@ async function crawlingReview() {
     // const browser = await puppeteer.launch();
     // const page = await browswer.newPage();
 
+    const pageUrl = [];
     let keys = Object.keys(jsonData.data.bestSeller);
     //console.log(keys);
     jsonData.data.bestSeller.map((el) =>{
@@ -20,29 +21,43 @@ async function crawlingReview() {
         
         //console.log(booksSerialNumber); 
 
-        const pageUrl = 'https://product.kyobobook.co.kr/detail/' + booksSerialNumber;
-        console.log(pageUrl); // 리스트업 되어 있는 책들의 리뷰 데이터가 들어있는 URL
+        pageUrl.push('https://product.kyobobook.co.kr/detail/' + booksSerialNumber);
+        
         //visitUrl(pageUrl);
     })
-
+    // console.log(pageUrl); // 리스트업 되어 있는 책들의 리뷰 데이터가 들어있는 URL
+    visitUrl(pageUrl[0]);
 }
 
 async function visitUrl (url) {
     const browser = await puppeteer.launch(); // ❶ 헤드리스 브라우저 실행
     const page = await browser.newPage();     // ❷ 브라우저에 새 페이지 생성
   
-    const pageUrl = "https://product.kyobobook.co.kr/bestseller/total?period=004";
-    await page.goto(pageUrl, {
+    // #ReviewList1 > div.tab_wrap.type_sm > div.tab_content > div > div.comment_list > div:nth-child(1) > div.comment_contents > div > div > div > div
+    await page.goto(url, {
       // ❸ 모든 네트워크 연결이 500ms 이상 유휴 상태가 될 때까지 기다림
       waitUntil: 'networkidle0'
     });
 
     const body = await page.content();
   
+    await page.waitForSelector('#ReviewList1 > div.tab_wrap.type_sm > div.tab_content > div > div.comment_list');
+
     await page.close();
     await browser.close();
 
-}
-crawlingReview();
+    const $ = cheerio.load(body);
 
-// #ReviewList1 > div.tab_wrap.type_sm > div.tab_content > div > div.pagination > div > a:nth-child(1)
+    const list = $('#ReviewList1 > div.tab_wrap.type_sm > div.tab_content > div > div.comment_list > div');
+
+    list.each((idx,el) =>{
+        const reviewText = $(el).find('div > div > div > div.comment_text').text();
+        const reviewPoint = $(el).find('div.comment_header > div.right_area > div > div > div.caption > span').text().split("");
+        //src = $(el).find('a').attr('href');
+        //grade = $(el).find('div.prod_bottom > div > span.review_klover_box > span.review_klover_text.font_size_xxs').text();
+        console.log(reviewText, 'point : ', reviewPoint);
+    });
+}
+
+crawlingReview();
+// #ReviewList1 > div.tab_wrap.type_sm > div.tab_content > div > div.comment_list > div:nth-child(10) > div.comment_header > div.right_area > div > div > div.caption > span
